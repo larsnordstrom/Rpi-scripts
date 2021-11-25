6c12e618ce0a1edcd49a283b5268de3c2e84070b
larsnordstrom <lars.nordstrom@jgy.se>
2021-11-25 10:45:44 +0100

    nya scripts

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ source scripts/system-update.sh
 source scripts/install-apache.sh
 source scripts/install-mariadb.sh
 source scripts/install-wordpress.sh
+source scripts/install-phpmyadmin.sh
+source scripts/setup-wp-db.sh
+
 
 # Skriv ut IP
 

28ca7c1ed1ba153dedd0d6a1873a1d56a45d939a
larsnordstrom <lars.nordstrom@jgy.se>
2021-11-25 10:47:49 +0100

    Formatering.

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -9,8 +9,11 @@ sudo apt-get install php -y
 
 sudo apt-get install php-mysql -y
 
+# Rewrite för länkar ska fungera korrekt i wordpress
 sudo a2enmod rewrite
+
 # Lägger till texten på första raden.
 sudo sed -i '1i <Directory /var/www/html>\n\tOptions Indexes FollowSymLinks MultiViews\n\tAllowOverride All\n\tRequire all granted\n</Directory>\n\n' /etc/apache2/sites-available/000-default.conf
 
+# Startar om apache service 
 sudo service apache2 restart

73ee959c4cad74b496efb640f77c2cc3fa1de4b1
larsnordstrom <lars.nordstrom@jgy.se>
2021-11-25 10:47:13 +0100

    modifierad med variabler och kontroller

--- scripts/install-wordpress.sh
+++ scripts/install-wordpress.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # sudo chmod 755 install-wordpress.sh
 
-sudo rm /var/www/html/*
+# Variabler
+WP_URL="http://wordpress.org/latest.tar.gz"
+WP_CONFIG="/var/www/html/wp-config.php"
+HTML_DIR="/var/www/html"
 
-cd /var/www/html
+sudo rm "$HTML_DIR/*"
 
-sudo wget http://wordpress.org/latest.tar.gz
+# Byter till HTML_DIR
+cd $HTML_DIR
+
+# Laddar ner Wordpress.
+sudo wget $WP_URL
 
 sudo tar xzf latest.tar.gz
 
@@ -13,8 +20,16 @@ sudo mv wordpress/* .
 
 sudo rm -rf wordpress latest.tar.gz
 
-# Permissions
-source set-permissions.sh
+# Ändrar så pi har rättigheter för html dir.
+sudo chown -R $USER:www-data $HTML_DIR
+sudo chmod -R 775 $HTML_DIR
 
-# Lägger till så uppdateringar fungerar i wordpress.
-echo "define( 'FS_METHOD', 'direct' );" | sudo tee -a /var/www/html/wp-config.php
+
+# Kollar om define( 'FS_METHOD', 'direct' ); redan finns i wp-config.php
+if grep -q "define( 'FS_METHOD', 'direct' );" $WP_CONFIG; then
+    echo "Hello world...."
+else
+    # Lägger till så uppdateringar fungerar i wordpress.
+    echo "define( 'FS_METHOD', 'direct' );" | sudo tee -a $WP_CONFIG
+fi
+

83bb3dee52508a6fb729ba73c0649c24c15ee173
larsnordstrom <lars.nordstrom@jgy.se>
2021-12-06 16:32:00 +0100

    WIP

--- scripts/install-oled.sh
+++ scripts/install-oled.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-
+################################
+# TODO:
+# Fixa i2c
+# Kontrollera om autostart filen finns
+#
+# Gör så autostart scriptet körs vid boot.
+#
+################################
 sudo pip3 install adafruit-circuitpython-ssd1306
 
-sudo python oled/oled_stats.py
 
+# Aktivera i2c
+
+
+# Skapa ett autostart script i Documents som kör oled skärmens python script.
+echo "#!/bin/bash\nsudo python /Rpi-scripts/scripts/oled/oled_stats.py" >> /Documents/my_autostart_script.sh
+
+# Gör scriptet körbart
+sudo chmod +x my_autostart_script.sh
